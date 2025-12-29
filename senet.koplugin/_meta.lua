be9ddb637bf61775e64e7103c314b628cf32b496
Chirheaux <37458780+xconny@users.noreply.github.com>
2025-12-29 12:27:24 +0100

    Create _meta.lua

---
+++ senet.koplugin/_meta.lua
@@ -0,0 +1,9 @@
+local _ = require("gettext")
+
+return {
+    name = "senet",
+    fullname = _([[Senet]]),
+    description = _([[Two-player ancient Egyptian race and capture game on a 30-house board. Throw the four sticks, move your pieces, respect the special houses, and be the first to escape with all five pieces.]]),
+    version = "1.0.0",
+    author = "chirheaux",
+}
